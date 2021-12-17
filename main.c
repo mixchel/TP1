2c8fe19f75045a00982e71fd06cb63540b13878a
mixchel <michelccs2003@gmail.com>
2021-12-17 17:46:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 void opera_lista(int tam_vetor, int num_teste, int qntd_de_operacoes, int *tipo_de_operacao, int *reps_da_operacao);
 
 int main() {
-    int num_teste, tam_vetor, tipo_de_operacao[ops_por_arq], reps_da_operacao[ops_por_arq], qntd_linhas;
+    int num_teste, tam_vetor, tipo_de_operacao[ops_por_arquivo], reps_da_operacao[ops_por_arquivo], qntd_linhas;
     char tipo_da_entrada;
     char nome_arquivo[tam_nome_arquivo];
 
@@ -26,7 +26,7 @@ int main() {
         printf("Quantas vezes a operacao sera feita:\n");
         scanf("%d", reps_da_operacao);
 
-        opera_lista(tam_vetor, num_teste, ops_por_term, tipo_de_operacao, reps_da_operacao);
+        opera_lista(tam_vetor, num_teste, ops_por_terminal, tipo_de_operacao, reps_da_operacao);
         
     } else if (tipo_da_entrada == 'a'){
 
@@ -37,16 +37,20 @@ int main() {
 
         FILE *fileptr;
         fileptr = fopen(nome_arquivo, "r");
+        if (fileptr != NULL){
+            fscanf(fileptr, "%d", &tam_vetor);
+            fscanf(fileptr, "%d", &qntd_linhas);
+            for (int i = 0; i < qntd_linhas; ++i) {
 
-        fscanf(fileptr, "%d", &tam_vetor);
-        fscanf(fileptr, "%d", &qntd_linhas);
-        for (int i = 0; i < qntd_linhas; ++i) {
+                fscanf(fileptr, "%d %d", &tipo_de_operacao[i], &reps_da_operacao[i]);
+            }
 
-            fscanf(fileptr, "%d %d", &tipo_de_operacao[i], &reps_da_operacao[i]);
+            opera_lista(tam_vetor, num_teste, qntd_linhas, tipo_de_operacao, reps_da_operacao);
+            fclose(fileptr);
         }
-
-        opera_lista(tam_vetor, num_teste, qntd_linhas, tipo_de_operacao, reps_da_operacao);
-        fclose(fileptr);
+        else {
+            printf("ERRO: Arquivo nao encontrado!");
+        }
     } 
     else {
         printf("ERRO: Entrada invalida!");
@@ -60,9 +64,11 @@ void opera_lista(int tam_vetor, int num_teste, int qntd_de_operacoes, int *tipo_
 
     lista_processo *lista_teste;
     lista_teste = (lista_processo*) malloc(sizeof(lista_processo));
-
     cria_lista(lista_teste, tam_vetor);
-    
+
+    FILE *ptrarquivo_saida;
+    ptrarquivo_saida = fopen("saida.txt","w");
+
     for (int i = 0; i < qntd_de_operacoes; i++) {   
 
         clock_t tempo_inicial = clock();
@@ -75,7 +81,7 @@ void opera_lista(int tam_vetor, int num_teste, int qntd_de_operacoes, int *tipo_
                 processo_teste = initprocesso();
                 insere_ordenado(lista_teste, *processo_teste);
             }
-            // imprime_lista(lista_teste);
+            imprime_lista(lista_teste);
         }
         else if (tipo_de_operacao[i] == 1) {
 
@@ -83,7 +89,7 @@ void opera_lista(int tam_vetor, int num_teste, int qntd_de_operacoes, int *tipo_
 
                 retira_primeiro(lista_teste);
             } 
-            // imprime_lista(lista_teste);
+            imprime_lista(lista_teste);
         }
             else {
 
@@ -92,6 +98,7 @@ void opera_lista(int tam_vetor, int num_teste, int qntd_de_operacoes, int *tipo_
 
         clock_t tempo_final = clock();
         double tempo_diferenca = (double)(tempo_final - tempo_inicial) / CLOCKS_PER_SEC;
-        printf("Teste %d: %lf segundos\n\n", num_teste+i, tempo_diferenca);  
+        fprintf(ptrarquivo_saida, "Teste %d: %lf segundos\n\n", num_teste+i, tempo_diferenca);
     }
+    fclose(ptrarquivo_saida);
 }
